36f394a460e8c1e472b55b5b33e6c9a49b1e1ada
Fl0rzinha <gabriel.florindo.souza46@gmail.com>
2024-01-29 17:20:48 -0300

    .

--- contato/script.js
+++ contato/script.js
@@ -18,3 +18,17 @@ function go_front(){
     form.style.boxShadow = "3px 3px 5px 0px var(--background-input)"
     form.reset();
 }
+
+function activate() {
+    let list = document.getElementById("list-link-header");
+
+    list.classList.add('options-list-activate');
+    list.classList.remove('options-list-header');
+}
+
+function desactivate() {
+    let list = document.getElementById("list-link-header");
+
+    list.classList.add('options-list-header');
+    list.classList.remove('options-list-activate');
+}
